95584b0b2df7f2738f35897fefed62923b0c6bbb
rafah <rafah5743@gmail.com>
2025-04-09 18:14:45 +0300

    finish crud direction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             FeatureSeeder::class,
             LanguageSeeder::class,
+            DirectionsSeeder::class,
         ]);
     }
 }
