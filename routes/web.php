6ff31d11ce207c5593426f230988e2142b21eaed
rafah <rafah5743@gmail.com>
2025-05-01 16:11:21 +0300

    add reservation for tenant

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MyFatoorahController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pay', [MyFatoorahController::class, 'index']);
+Route::get('/callback', [MyFatoorahController::class, 'callback'])->name('myfatoorah.callback');
+Route::get('/checkout', [MyFatoorahController::class, 'checkout']);
+Route::post('/webhook', [MyFatoorahController::class, 'webhook']);
